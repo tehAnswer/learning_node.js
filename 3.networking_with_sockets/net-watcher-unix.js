8eecee0a76828d6e03931eba5310806f8d2cccf7
Sergio G <sergiorodriguezgijon@gmail.com>
2015-07-27 17:02:50 +0200

    Writting data to a unix socket

---
+++ 3.networking_with_sockets/net-watcher-unix.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const
+  fs = require('fs'),
+  net = require('net'),
+  filename = process.argv[2],
+  server = net.createServer(function(connection) {
+    console.log('Suscribber connected');
+    connection.write('Watching ' + filename + " for changes!\n");
+    
+    let watcher = fs.watch(filename, function() {
+     connection.write('Change in ' + Date.now() + ".\n");
+    });
+    
+    connection.on('close', function() {
+      console.log('Suscriber disconnected');
+      watcher.close();
+    });
+  });
+
+if (!filename) {
+  throw Error('No filename');
+}
+
+server.listen('/tmp/watcher.sock', function() {
+  console.log('Listening...');
+});
