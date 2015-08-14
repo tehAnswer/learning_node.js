93450e30f16e0f40085e837da6c0e5b3683e26bc
Sergio G <sergiorodriguezgijon@gmail.com>
2015-08-14 14:03:47 +0200

    Parsing XML Data (RDF)

---
+++ 5.accessing_databases/rdf-parser.js
@@ -0,0 +1,32 @@
+'use strict';
+ const
+  fs = require('fs'),
+  cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if(err) {
+      return callback(err);
+    } 
+    let
+      $ = cheerio.load(data.toString(), { xmlMode: true }),
+      authorsList = [],
+      subjectList = [],
+      pushAuthors = function(index, element) {
+        authorsList.push($(element).text());
+      },
+      pushSubjects = function(index, element) {
+        subjectList.push($(element).text());
+      };
+
+      $('pgterms\\:agent pgterms\\:name').map(pushAuthors);
+      $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(pushSubjects);
+
+     callback(null, {
+       _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebook/', ''),
+       title: $('dcterms\\:title').text(),
+       authors: authorsList,
+       subjects: subjectList
+     });
+  });
+};
