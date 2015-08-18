1f0f4233cef2583772a589b6b8175771479794ad
Sergio G <sergiorodriguezgijon@gmail.com>
2015-08-18 17:54:30 +0200

    Working with generators

--- 6.scalable_web_services/b4/lib/bundle.js
+++ 6.scalable_web_services/b4/lib/bundle.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const
-  Q = require('q'),
-  request = require('request');
+var Q = require('q');
+var request = require('request');
 
 var resolvePromise = function(err, dbRes, body, promise) {
   if(err) {
@@ -71,5 +70,47 @@ module.exports = function(config, app) {
     var callbacks = getResponseCallbacks(res);
     var result = promiseChain.then(callbacks[0], callbacks[1]);
     result.done();
+  });
+
+  app.put('/api/bundle/:id/book/:pgid', function(req, res) {
+    
+    let
+      get = Q.denodeify(request.get), 
+      put = Q.denodeify(request.put);
+    
+    Q.async(function* (){  
+      let args, couchRes, bundle, book;
+      
+      // grab the bundle from the b4 database
+      args = yield get(config.b4db + req.params.id);  
+      couchRes = args[0];
+      bundle = JSON.parse(args[1]);
+      
+      // fail fast if we couldn't retrieve the bundle
+      if (couchRes.statusCode !== 200) {
+        res.status(couchRes.statusCode).json(bundle);
+        return;
+      }
+      
+      // look up the book by its Project Gutenberg ID
+      args = yield get(config.bookdb + req.params.pgid);  
+      couchRes = args[0];
+      book = JSON.parse(args[1]);
+      
+      // fail fast if we couldn't retrieve the book
+      if (couchRes.statusCode !== 200) {
+        res.status(couchRes.statusCode).json(book);
+        return;
+      }
+      
+      // add the book to the bundle and put it back in CouchDB
+      bundle.books[book._id] = book.title;  
+      args = yield put({url: config.b4db + bundle._id, json: bundle});
+      res.json(args[0].statusCode, args[1]);
+      
+    })()  
+    .catch(function(err) {  
+      res.json(502, { error: "bad_gateway", reason: err.code });
+    });
   });
 };
