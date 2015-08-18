376f71220b97dd97bcfd348262036bdd0f1ed5ed
Sergio G <sergiorodriguezgijon@gmail.com>
2015-08-18 16:07:41 +0200

    PUT route and fix some bugs

--- 6.scalable_web_services/b4/lib/bundle.js
+++ 6.scalable_web_services/b4/lib/bundle.js
@@ -4,7 +4,7 @@ const
   Q = require('q'),
   request = require('request');
 
-var callback = function(err, dbRes, body, promise) {
+var resolvePromise = function(err, dbRes, body, promise) {
   if(err) {
     promise.reject(err);
   } else {
@@ -23,28 +23,53 @@ var queryParams = function(config, request) {
   };
 };
 
+function getResponseCallbacks(response) {
+  var successHook = function(args) {
+    var couchRes = args[0];
+    var body = JSON.parse(JSON.stringify(args[1]));
+    response.status(couchRes.statusCode).json(body);
+  };
+
+  var failureHook = function(err) {
+    response.status(502).json({ "error" : "bad_gateway" }); 
+  }
+
+  return [successHook, failureHook];
+};
+
 module.exports = function(config, app) {
   app.post('/api/bundle', function(req, res) {
     var deferred = Q.defer();
     request.post(queryParams(config, req), function (err, dbRes, body) {
-      callback(err, dbRes, body, deferred);
-      deferred.promise.then(function(args) {
-        let couchRes = args[0], body = args[1];
-        res.json(couchRes.statusCode, body);
-      }, function(err) {
-        res.json(502, { "error" : "bad_gateway" , "reason": err.code }); 
-      });
+      resolvePromise(err, dbRes, body, deferred);
+      var callbacks = getResponseCallbacks(res);
+      deferred.promise.then(callbacks[0], callbacks[1]);
     });
   });
 
   app.get('/api/bundle/:id', function(req, res) {
     var promise = Q.nfcall(request.get, config.b4db + '/' + req.params.id);
-    var result = promise.then(function(args) {
-      let couchRes = args[0], body = JSON.parse(args[1]);
-      res.json(couchRes.statusCode, body);
-    }, function(err) {
-      res.json(502, { error: "bad_gateway", reason: err.code })
+    var callbacks = getResponseCallbacks(res);
+    var result = promise.then(callbacks[0], callbacks[1]);
+    result.done();
+  });
+
+  app.put('/api/bundle/:id/name/:name', function(req, res) {
+    var promise = Q.nfcall(request.get, config.b4db + '/' + req.params.id);
+    var promiseChain = promise.then(function(args) {
+      let couchRes = args[0], bundle = JSON.parse(args[1]);
+      if(couchRes.statusCode != 200) {
+        return [couchRes, bundle];
+      }
+
+      bundle.name = req.params.name;
+      return Q.nfcall(request.put, {
+        url: config.b4db + '/' + req.params.id,
+        json: bundle
+      });
     });
+    var callbacks = getResponseCallbacks(res);
+    var result = promiseChain.then(callbacks[0], callbacks[1]);
     result.done();
   });
 };
